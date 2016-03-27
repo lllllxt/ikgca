1056c22186b9e1bfdda1fdab869a0355531de71b
liuxiaotang <532543299@qq.com>
2016-03-28 01:28:03 +0800

    2016-03-28
    +页面加载loading效果
    下次更新各信息编辑页必填星号

--- js/index.js
+++ js/index.js
@@ -1,37 +1,45 @@
-/**
+﻿/**
  * Created by SexLiu on 2016/1/13.
  */
-function home(){
+function home() {
+    openLoading();
     $('#body').load('home.html');
 }
 function loadUserList() {
+    openLoading();
     $('#body').load('user-list.html');
 }
 function loadToolList() {
+    openLoading();
     $('#body').load('tool-list.html');
 }
 function loadToolFlowList() {
+    openLoading();
     $('#body').load('tool-flow.html');
 }
 function loadHomeRepairList() {
+    openLoading();
     $('#body').load('home-repair-list.html');
 }
 function loadNoticeList() {
+    openLoading();
     $('#body').load('notice-list.html');
 }
 function loadActivityList() {
+    openLoading();
     $('#body').load('activity-list.html');
 }
 function loadLottery() {
+    openLoading();
     $('#body').load('lottery.html');
 }
 
 //页面效果
-$('#menu li').click(function(){
+$('#menu li').click(function () {
     $('#menu li').removeClass('active');
     $(this).addClass('active');
 });
-$('[onclick]').click(function(){
+$('[onclick]').click(function () {
     $(".navbar-collapse").removeClass('in');
 });
 $(function () {
@@ -39,16 +47,23 @@ $(function () {
 });
 
 //判断是否登录，没登录就跳转到登录页
-if(sessionStorage.loginId){
+if (sessionStorage.loginId) {
     $('#loginName').html(sessionStorage.loginName);
-}else{
-    window.location="../view/login.html";
+} else {
+    window.location = "../view/login.html";
 }
 
-function signOut(){
+function signOut() {
     sessionStorage.clear();
 }
 
-function changePassword(){
+function changePassword() {
     $('#body').load('password-edit.html');
+}
+
+function openLoading() {
+    $('#loading').removeClass("hidden");
+}
+function closeLoading() {
+    $('#loading').addClass("hidden");
 }
